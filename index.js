57bfd433a390110bf94fc62707b0c2d13acd9040
alec <kaltschmidalec@gmail.com>
2021-07-21 12:47:28 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(arr, name) {
+    name = name.toUpperCase()
+    return arr.filter( function(x) {return x.toUpperCase() === name})
+}
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(el => el.startsWith(str))
+}
+
+function matchName(arr, string) {
+    return arr.filter(el => el.name === string)
+}
