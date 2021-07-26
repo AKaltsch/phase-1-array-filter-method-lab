c99aa12db58b485a6ceac14648f75066226d1e5b
alec <kaltschmidalec@gmail.com>
2021-07-25 20:00:17 -0700

    commit tree 3bbe37e9bfdf64788df1f957dd8ff651271a5a74

--- index.js
+++ index.js
@@ -2,15 +2,60 @@
 
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
-function findMatching(arr, name) {
-    name = name.toUpperCase()
-    return arr.filter( function(x) {return x.toUpperCase() === name})
+// function findMatching(arr, name) {
+//     name = name.toUpperCase()
+//     return arr.filter( function(x) {return x.toUpperCase() === name})
+// }
+
+// function fuzzyMatch(arr, str) {
+//     return arr.filter(el => el.startsWith(str))
+// }
+
+// function matchName(arr, string) {
+//     return arr.filter(el => el.name === string)
+// }
+
+function findMatching(array, string) {
+    string  = string.toLowerCase()
+    return array.filter(name => name.toLowerCase() === string)
 }
 
-function fuzzyMatch(arr, str) {
-    return arr.filter(el => el.startsWith(str))
+function fuzzyMatch(array, str) {
+    return array.filter(name => name.startsWith(str))
 }
 
-function matchName(arr, string) {
-    return arr.filter(el => el.name === string)
+function matchName(arr, str) {
+    return arr.filter(el => el.name === str)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
